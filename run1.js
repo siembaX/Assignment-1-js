3b3ffeab02a897f4dbcbc798e2248b8790e45628
Simba Blessed <simbamahla@gmail.com>
2023-05-09 09:15:39 +0200

    first commit

---
+++ run1.js
@@ -0,0 +1,46 @@
+//Part 1 & 2
+
+const PI = 3.14
+let my_name = 'Shifu'
+let age = 1000
+
+console.log(`My name is ${my_name} and I am ${age} years old.`)
+
+//Part 3
+
+    const calc_num = (a, b) => {
+    a= 2 ; b=3
+   result = a + b
+   console.log(result)
+}
+console.log(calc_num())
+
+//PART 4
+
+function sum (a=1, b=2){
+   
+result= a+b
+console.log(result)
+}
+console.log(sum())
+
+//Part 5
+
+var named = 'John'
+var greeting = ''
+console.log(`Hello ${named}`)
+
+//Part 6
+
+function greetings(){
+    var named = 'John'
+    console.log(`Heyyyy ${named}`)
+}
+
+greetings()
+
+
+
+
+
+  
